89b7863e6c40351ceb2471f5e48c1132fd25603d
Paweł Żemła <pawzem@gmail.com>
2020-09-20 21:38:11 +0200

    + empty tests for  domain class + empty resources

---
+++ src/test/java/com/pz/reservoir/company/CompanyFacadeTest.java
@@ -0,0 +1,29 @@
+package com.pz.reservoir.company;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyFacadeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addCompany() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void addUnit() {
+    }
+
+    @Test
+    void addWorkstation() {
+    }
+}
